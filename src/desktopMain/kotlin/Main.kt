0084883fa5af0f42a6645af1bcea0b7dc9e0d8f7
dumbgreenfish <dumbgreenfish@proton.me>
2026-05-06 00:28:55 +0500

    Scpecified minimum window size

--- src/desktopMain/kotlin/Main.kt
+++ src/desktopMain/kotlin/Main.kt
@@ -4,14 +4,16 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import ru.greenfish.dialogueforge.App
+import java.awt.Dimension
 
 fun main() = application {
-    val state = rememberWindowState(size = DpSize(640.dp, 480.dp))
+    val state = rememberWindowState(size = DpSize(800.dp, 600.dp))
     Window(
         onCloseRequest = ::exitApplication,
         title = "DialogueForge",
         state = state
     ) {
+        window.minimumSize = Dimension(600, 450)
         App()
     }
 }
